df44f2caa152a005c8d76edfb567a3fe116cd9c8
Riyan Amanda <ryant.n92@gmail.com>
2022-11-27 02:22:21 +0700

    add sticky navbar

--- src/components/organisms/header.js
+++ src/components/organisms/header.js
@@ -1,45 +1,62 @@
 import { navigations } from '@/data';
-import React from 'react';
 
 const Header = () => {
-    return (
-        <header className='container px-[1rem] py-5 flex flex-col md:flex-row justify-center md:justify-between'>
-            <nav>
-                <ul className='flex items-center justify-center text-xs space-x-4 list-inside list-decimal'>
-                    {navigations.map((navigation, index) => (
-                        <li
-                            key={index}
-                            className='group text-purple-500 transition-all duration-300'
-                            data-aos='zoom-in'
-                            data-aos-delay={index * 100}
-                            data-aos-duration='1000'
-                        >
-                            <a
-                                href={navigation.url}
-                                target={'_blank'}
-                                rel={'noreferrer'}
-                                className='text-black group-hover:text-purple-500 transition-all duration-300'
-                            >
-                                {navigation.name}
-                            </a>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
+    const scroll = () => {
+        const header = document.getElementById('header');
+        let height = window.scrollY;
+        if (height > 100) {
+            header.classList.add('shadow', 'bg-white/50');
+        } else {
+            header.classList.remove('shadow', 'bg-white/50');
+        }
+    };
 
-            <a
-                href={'/docs/new-cv.pdf'}
-                target={'_blank'}
-                rel={'noreferrer'}
-                data-aos='zoom-in'
-                data-aos-delay='500'
-                data-aos-duration='1000'
-                className='text-center w-fit self-center'
-            >
-                <button className='bg-purple-500 text-white text-xs px-3 py-1.5 mt-8 md:mt-0 rounded shadow-xl shadow-purple-500/10 font-medium hover:bg-purple-100 transition-all duration-300 hover:text-purple-500'>
-                    Resume
-                </button>
-            </a>
+    if (typeof window !== 'undefined')
+        window.addEventListener('scroll', scroll);
+
+    return (
+        <header
+            id='header'
+            className='sticky left-0 top-0 z-50 backdrop-blur transition-all duration-500'
+        >
+            <div className='container px-[1rem] py-5 flex flex-col md:flex-row justify-center md:justify-between md:items-center'>
+                <nav>
+                    <ul className='flex items-center justify-center text-xs space-x-4 list-inside list-decimal'>
+                        {navigations.map((navigation, index) => (
+                            <li
+                                key={index}
+                                className='group text-purple-500 transition-all duration-300'
+                                data-aos='zoom-in'
+                                data-aos-delay={index * 100}
+                                data-aos-duration='1000'
+                            >
+                                <a
+                                    href={navigation.url}
+                                    target={'_blank'}
+                                    rel={'noreferrer'}
+                                    className='text-black group-hover:text-purple-500 transition-all duration-300'
+                                >
+                                    {navigation.name}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+
+                <a
+                    href={'/docs/new-cv.pdf'}
+                    target={'_blank'}
+                    rel={'noreferrer'}
+                    data-aos='zoom-in'
+                    data-aos-delay='500'
+                    data-aos-duration='1000'
+                    className='text-center w-fit self-center'
+                >
+                    <button className='bg-purple-500 text-white text-xs px-3 py-1.5 mt-8 md:mt-0 rounded shadow-xl shadow-purple-500/10 font-medium hover:bg-purple-100 transition-all duration-300 hover:text-purple-500'>
+                        Resume
+                    </button>
+                </a>
+            </div>
         </header>
     );
 };
